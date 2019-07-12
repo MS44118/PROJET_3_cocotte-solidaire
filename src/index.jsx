d24122914793fb1ba4a579a034751638c310782d
AureliaRoumesy <aurelia.roumesy@gmail.com>
2019-07-12 16:31:10 +0200

    add icon filter on user page

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ import 'moment/locale/fr';
 
 const store = createStore(
   rootReducer,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
 ReactDOM.render(
